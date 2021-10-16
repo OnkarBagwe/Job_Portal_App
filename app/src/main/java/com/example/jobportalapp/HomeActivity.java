d58366f0f50038969cf83143cf9c9fe4c1a04e7c
Nishant Dave <davenishant2k@gmail.com>
2021-10-17 01:40:33 +0530

    added toolbar

--- app/src/main/java/com/example/jobportalapp/HomeActivity.java
+++ app/src/main/java/com/example/jobportalapp/HomeActivity.java
@@ -2,13 +2,46 @@ package com.example.jobportalapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.widget.Toolbar;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private Button btnAlljob;
+    private Button btnPostjob;
+
+    //Toolbar
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+//
+//        toolbar=findViewById(R.id.toolbar_home);
+//        setSupportActionBar(toolbar);
+//        getSupportActionBar().setTitle("Job Portal App");
+        btnAlljob = findViewById(R.id.btn_allJob);
+        btnPostjob = findViewById(R.id.btn_postJob);
+
+        btnAlljob.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), AllJobActivity.class));
+            }
+        });
+
+        btnPostjob.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), PostJobActivity.class));
+
+            }
+        });
     }
 }
