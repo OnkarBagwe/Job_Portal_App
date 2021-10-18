72a6069b51607b88d733b3154009eb2752428f09
OnkarBagwe <onkarbagwe25@gmail.com>
2021-10-18 15:08:09 +0530

    31_videos

--- app/src/main/java/com/example/jobportalapp/AllJobActivity.java
+++ app/src/main/java/com/example/jobportalapp/AllJobActivity.java
@@ -1,14 +1,108 @@
 package com.example.jobportalapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.w3c.dom.Text;
 
 public class AllJobActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+    //Recycler
+    private RecyclerView recyclerView;
+
+    //Firebase
+
+    private DatabaseReference mAllJobPost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_job);
+
+        toolbar=findViewById(R.id.all_job_post);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("All Job Post");
+
+        //Database
+
+        mAllJobPost= FirebaseDatabase.getInstance().getReference().child("Job Post");
+        mAllJobPost.keepSynced(true);
+
+        recyclerView=findViewById(R.id.recycler_all_job);
+
+        LinearLayoutManager layoutManager=new LinearLayoutManager(this);
+        layoutManager.setReverseLayout(true);
+        layoutManager.setStackFromEnd(true);
+
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(layoutManager);
+
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+
+        FirebaseRecyclerAdapter<Data, AllJobPostViewHolder>adapter=new FirebaseRecyclerAdapter<Data, AllJobPostViewHolder>
+                (
+                       Data.class,
+                       R.layout.alljobpost,
+                       AllJobPostViewHolder.class,
+                       mAllJobPost
+
+                ) {
+            @Override
+            protected void populateViewHolder(AllJobPostViewHolder allJobPostViewHolder, Data data, int i) {
+
+            }
+        }
+    }
+
+    public static class AllJobPostViewHolder extends RecyclerView.ViewHolder{
+
+        View myview;
+
+        public AllJobPostViewHolder(View itemView){
+            super(itemView);
+            myview=itemView;
+        }
+
+        public void setJobTitle(String title){
+            TextView mTitle=myview.findViewById(R.id.all_job_post_title);
+            mTitle.setText(title);
+        }
+
+        public void setJobDate(String date){
+            TextView mDate=myview.findViewById(R.id.all_job_post_date);
+            mDate.setText(date);
+        }
+
+        public void setJobDescription(String description){
+            TextView mDescription=myview.findViewById(R.id.all_job_post_description);
+            mDescription.setText(description);
+        }
+
+        public void setJobSkills(String skills){
+            TextView mSkills=myview.findViewById(R.id.all_job_post_skills);
+            mSkills.setText(skills);
+        }
+
+        public void setJobSalary(String salary){
+            TextView mSalary=myview.findViewById(R.id.all_job_post_salary);
+            mSalary.setText(salary);
+        }
+
     }
 }
