4155ce633b697d75c5b79dff6d6514c83790e9ae
OnkarBagwe <onkarbagwe25@gmail.com>
2021-10-24 15:15:13 +0530

    logout done

--- app/src/main/java/com/example/jobportalapp/HomeActivity.java
+++ app/src/main/java/com/example/jobportalapp/HomeActivity.java
@@ -4,10 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.widget.Toolbar;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -18,6 +22,8 @@ public class HomeActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
 
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +32,9 @@ public class HomeActivity extends AppCompatActivity {
 //        toolbar=findViewById(R.id.toolbar_home);
 //        setSupportActionBar(toolbar);
 //        getSupportActionBar().setTitle("Job Portal App");
+
+        mAuth=FirebaseAuth.getInstance();
+
         btnAlljob = findViewById(R.id.btn_allJob);
         btnPostjob = findViewById(R.id.btn_postJob);
 
@@ -43,5 +52,27 @@ public class HomeActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.mainmenu,menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.logout:
+                mAuth.signOut();
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                break;
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
